838c7b601f662a22cea6bcfbafd28ee8711ef8b6
BrundaMBharadwaj <brundabharadwaj22@gmail.com>
2023-03-03 17:13:53 +0530

    removed warnings

--- quix-ui/src/Components/SignUP/SignUp.jsx
+++ quix-ui/src/Components/SignUP/SignUp.jsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { Card,Icon, Image } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 import Form from "semantic-ui-react/dist/commonjs/collections/Form";
  import Logo from '../Assets/quiz-buzz-logo.png'
-const items = [
-  {
-    header: 'Project Report - April',
-    description:
-      'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
-    meta: 'ROI: 30%',
-  },
-  {
-    header: 'Project Report - May',
-    description:
-      'Bring to the table win-win survival strategies to ensure proactive domination.',
-    meta: 'ROI: 34%',
-  },
-]
+
 
  const 
     SignUp = () => {  return(
@@ -25,7 +12,7 @@ const items = [
       <Card centered>
         <Card.Content>
           <Card.Header>Sign UP</Card.Header>
-          <Card.Meta><a>Login instead? </a></Card.Meta>
+          <Card.Meta>Login instead? </Card.Meta>
           <Card.Description>
             <Form  >
                 <Form.Field></Form.Field>

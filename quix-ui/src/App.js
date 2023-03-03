8cd12f81ee67813cb1fc1cc67fa300ce2f570f74
BrundaMBharadwaj <brundabharadwaj22@gmail.com>
2023-03-03 17:17:47 +0530

    removed warnings

--- quix-ui/src/App.js
+++ quix-ui/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import Login from './Components/Login/Login';
 import SignUp from './Components/SignUP/SignUp';
